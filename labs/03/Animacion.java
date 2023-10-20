e147e827f648e7aaf65a8dc8bbac9e7f229cf5f6
juanntrs <141367365+juanntrs@users.noreply.github.com>
2023-10-19 21:47:57 -0500

    Add files via upload

--- labs/03/Animacion.java
+++ labs/03/Animacion.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 public class Animacion extends JFrame implements Runnable{
@@ -27,7 +29,7 @@ public class Animacion extends JFrame implements Runnable{
         setTitle("Animación");
         hilo= new Thread(this);
         Toolkit herramienta=Toolkit.getDefaultToolkit();
-        img=herramienta.getImage(getClass().getResource("/animation/personaje_1.png"));
+        img=herramienta.getImage(getClass().getResource("/animation/max.png"));
         hilo.start();
     }
 
@@ -35,7 +37,9 @@ public class Animacion extends JFrame implements Runnable{
     public void paint(Graphics g) {
        Graphics2D g2D;
        g2D= (Graphics2D)g;
-       g2D.drawImage(img,200,200,200+154,200+81,null,this);
+       int mx=(incremento%9)*80;
+       g2D.drawImage(img,200,200,200+80,200+140,mx,0,mx+80,140, null,this);
+       repaint();
     }
 
     
@@ -43,6 +47,13 @@ public class Animacion extends JFrame implements Runnable{
     
     @Override
     public void run() {
-      
+      while (true){
+          try {
+              Thread.sleep(70); //tiempo que dura una imagen antes de pasar a la siguiente
+          } catch (InterruptedException ex) {
+              Logger.getLogger(Animacion.class.getName()).log(Level.SEVERE, null, ex);
+          }
+          incremento=incremento+1;
+      }
     }
 }

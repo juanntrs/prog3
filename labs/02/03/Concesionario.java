6db45446640b65291f67d9f750a7794ad752cbd7
juanntrs <141367365+juanntrs@users.noreply.github.com>
2023-10-28 18:33:47 -0500

    Add files via upload

---
+++ labs/02/03/Concesionario.java
@@ -0,0 +1,12 @@
+package com.mycompany.concesionario;
+
+
+
+public class Concesionario {
+
+    public static void main(String[] args) {
+        InterfazC interfaz = new InterfazC();
+        interfaz.setVisible(true);
+        interfaz.setLocationRelativeTo(null);
+    }
+}

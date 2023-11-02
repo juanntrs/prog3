1a4d4cc54fa9633e0cee1843c1a1ff2f06e86c5c
juanntrs <141367365+juanntrs@users.noreply.github.com>
2023-11-02 15:31:42 -0500

    Add files via upload

---
+++ labs/06/01/CarreritaAutos/src/main/java/CarreritaAutos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class CarreritaAutos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

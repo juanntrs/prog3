b548ae21f6b8f6e0e56caa4159f5c73f4bc18e88
juanntrs <141367365+juanntrs@users.noreply.github.com>
2023-11-04 19:42:05 -0500

    Add files via upload

--- labs/06/01/CarreritaAutos/src/main/java/CarreritaAutos.java
+++ labs/06/01/CarreritaAutos/src/main/java/CarreritaAutos.java
@@ -1,14 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
 
-/**
- *
- * @author Estudiante
- */
-public class CarreritaAutos {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class CarreritaAutos extends Thread {
+    
+    private JLabel carro;
+    private Piques window;
+    private String nombreCarro;
+    
+    public CarreritaAutos (JLabel carro, Piques Window, String Nombre){
+        this.carro = carro;
+        this.window = window;
+        this.nombreCarro = Nombre;
+    }
+    
+    
+    
+    @Override
+    public void run(){
+        while(true){
+            try{
+                sleep((int) (Math.random()*1000));
+            } catch (InterruptedException ex){
+                Logger.getLogger(CarreritaAutos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
